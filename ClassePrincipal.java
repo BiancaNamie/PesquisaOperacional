f1b2d5b20f1049a9e1afc92b4149ee57090ce559
BiancaNamie <biancanamie@gmail.com>
2020-07-31 21:13:19 +0000

    Talvez eu coloque mais testes depois

--- ClassePrincipal.java
+++ ClassePrincipal.java
@@ -1,14 +1,20 @@
 public class ClassePrincipal{
 
     public static void main(String[] args) {
-        String maxMin = "min";
-        double funcaoObjetivo[] = {-1,-2};
-        double matrizA[][] = {{1,1},{1,-1},{-1,1}};
+       /* String maxMin = "min";
+        double funcaoObjetivo[] = {-1,-1};
+        double matrizA[][] = {{2,1},{-1,2},{3,-6}};
         String igualdades[] = {"<=","<=","<="};
-        double b[] = {6,4,4};
+        double b[] = {18,4,12};
+        */
+        String maxMin = "max";
+        double funcaoObjetivo[] = {4,3};
+        double matrizA[][] = {{1,3},{2,2},{1,1},{0,1}};
+        String igualdades[] = {"<=","=","<=","<="};
+        double b[] = {7,8,-3,2};
 
         Simplex2Fases simplex = new Simplex2Fases(maxMin, funcaoObjetivo, matrizA, igualdades, b);
-        
+        simplex.imprimeResultado();
         /*MatrizInversa inversa = new MatrizInversa(matrizA);
 
         double mInversa[][] = new double[][];
