ec8fb06d092dce14cd5cf131f136ce698084d96c
BiancaNamie <biancanamie@gmail.com>
2020-08-09 21:20:52 +0000

    acrescentado mais testes

--- ClassePrincipal.java
+++ ClassePrincipal.java
@@ -7,11 +7,37 @@ public class ClassePrincipal{
         String igualdades[] = {"<=","<=","<="};
         double b[] = {18,4,12};
         */
-        String maxMin = "max";
+        /* String maxMin = "max";
         double funcaoObjetivo[] = {4,3};
         double matrizA[][] = {{1,3},{2,2},{1,1},{0,1}};
         String igualdades[] = {"<=","=","<=","<="};
         double b[] = {7,8,-3,2};
+        */
+        /*
+        String maxMin = "min";
+        double funcaoObjetivo[] = {-1,-2};
+        double matrizA[][] = {{1,1},{-5,2},{3,5}};
+        String igualdades[] = {">=",">=",">="};
+        double b[] = {0,-10,15};
+        */
+        /*
+        String maxMin = "max";
+        double funcaoObjetivo[] = {1,1};
+        double matrizA[][] = {{2,1},{-1,2},{3,6}};
+        String igualdades[] = {"<=","<=",">="};
+        double b[] = {18,4,-12};
+        */
+        /*String maxMin = "min";
+        double funcaoObjetivo[] = {4,-12};
+        double matrizA[][] = {{2,1},{1,3},{1,0}};
+        String igualdades[] = {">=","<=",">="};
+        double b[] = {6,8,4};
+        */
+        String maxMin = "max";
+        double funcaoObjetivo[] = {3,3,1};
+        double matrizA[][] = {{1,0,3},{0,1,0},{3,0,2},{1,1,0}};
+        String igualdades[] = {"<=","<=",">=","<="};
+        double b[] = {5,5,6,10};
 
         Simplex2Fases simplex = new Simplex2Fases(maxMin, funcaoObjetivo, matrizA, igualdades, b);
         simplex.imprimeResultado();
